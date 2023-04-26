f4544c2dce7eeeeb40c1a93007b60fe8c190f45d
Jethro Au <galongau@gmail.com>
2023-04-26 17:42:02 +0100

    initial commit

---
+++ base/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('room/<str:pk>/', views.room, name="room"),
+    path('create-room/', views.createRoom, name="create-room"),
+    path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
+    path('login/', views.loginView, name="login"),
+]
