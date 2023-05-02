40e1fa577331b4ff0586e6e2fdad57685364daa7
Jethro <galongau@gmail.com>
2023-05-02 20:12:46 +0100

    Update for deploy

--- studybud/settings.py
+++ studybud/settings.py
@@ -28,10 +28,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',
+    'https://studybud-jethro.up.railway.app'
+]
 
 # Application definition
 
